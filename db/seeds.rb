9fa87f39a12829daa24c35aaf6970d9c850f0243
Damian Janus <damian.janus@gmail.com>
2025-05-20 17:25:24 +0200

    add active storage for image files

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Movie.destroy_all
+
 Movie.create!([
   {
     title: 'Avengers: Endgame',
@@ -18,8 +20,7 @@ Movie.create!([
     rating: 'PG-13',
     total_gross: 1_223_641_414,
     director: 'Anthony Russo',
-    duration: '181 min',
-    image_file_name: 'avengers-end-game.png'
+    duration: '181 min'
   },
   {
     title: 'Captain Marvel',
@@ -31,8 +32,7 @@ Movie.create!([
     rating: 'PG-13',
     total_gross: 1_110_662_849,
     director: 'Anna Boden',
-    duration: '124 min',
-    image_file_name: 'captain-marvel.png'
+    duration: '124 min'
   },
   {
     title: 'Black Panther',
@@ -44,8 +44,7 @@ Movie.create!([
     rating: 'PG-13',
     total_gross: 1_346_913_161,
     director: 'Ryan Coogler',
-    duration: '134 min',
-    image_file_name: 'black-panther.png'
+    duration: '134 min'
   },
   {
     title: 'Avengers: Infinity War',
@@ -57,8 +56,7 @@ Movie.create!([
     rating: 'PG-13',
     total_gross: 2_048_359_754,
     director: 'Anthony Russo',
-    duration: '149 min',
-    image_file_name: 'avengers-infinity-war.png'
+    duration: '149 min'
   },
   {
     title: 'Green Lantern',
@@ -70,8 +68,7 @@ Movie.create!([
     rating: 'PG-13',
     total_gross: 219_851_172,
     director: 'Martin Campbell',
-    duration: '114 min',
-    image_file_name: 'green-lantern.png'
+    duration: '114 min'
   },
   {
     title: 'Fantastic Four',
@@ -83,8 +80,7 @@ Movie.create!([
     rating: 'PG-13',
     total_gross: 168_257_860,
     director: 'Josh Trank',
-    duration: '100 min',
-    image_file_name: 'fantastic-four.png'
+    duration: '100 min'
   },
   {
     title: 'Iron Man',
@@ -98,8 +94,7 @@ Movie.create!([
     rating: 'PG-13',
     total_gross: 585_366_247,
     director: 'Jon Favreau',
-    duration: '126 min',
-    image_file_name: 'ironman.png'
+    duration: '126 min'
   },
   {
     title: 'Superman',
@@ -113,8 +108,7 @@ Movie.create!([
     rating: 'PG',
     total_gross: 300_451_603,
     director: 'Richard Donner',
-    duration: '143 min',
-    image_file_name: 'superman.png'
+    duration: '143 min'
   },
   {
     title: 'Spider-Man',
@@ -129,8 +123,7 @@ Movie.create!([
     rating: 'PG-13',
     total_gross: 825_025_036,
     director: 'Sam Raimi',
-    duration: '121 min',
-    image_file_name: 'spiderman.png'
+    duration: '121 min'
   },
   {
     title: 'Batman',
@@ -143,8 +136,7 @@ Movie.create!([
     rating: 'PG-13',
     total_gross: 411_348_924,
     director: 'Tim Burton',
-    duration: '126 min',
-    image_file_name: 'batman.png'
+    duration: '126 min'
   },
   {
     title: "Catwoman",
@@ -156,8 +148,7 @@ Movie.create!([
     rating: "PG-13",
     total_gross: 82_102_379,
     director: "Jean-Christophe 'Pitof' Comar",
-    duration: "101 min",
-    image_file_name: "catwoman.png"
+    duration: "101 min"
   },
   {
     title: "Wonder Woman",
@@ -169,10 +160,28 @@ Movie.create!([
     rating: "PG-13",
     total_gross: 821_847_012,
     director: "Patty Jenkins",
-    duration: "141 min",
-    image_file_name: "wonder-woman.png"
+    duration: "141 min"
   }
 ])
+
+[
+  ["Avengers: Endgame", "avengers-end-game.png"],
+  ["Captain Marvel", "captain-marvel.png"],
+  ["Black Panther", "black-panther.png"],
+  ["Avengers: Infinity War", "avengers-infinity-war.png"],
+  ["Green Lantern", "green-lantern.png"],
+  ["Fantastic Four", "fantastic-four.png"],
+  ["Iron Man", "ironman.png"],
+  ["Superman", "superman.png"],
+  ["Spider-Man", "spiderman.png"],
+  ["Batman", "batman.png"],
+  ["Catwoman", "catwoman.png"],
+  ["Wonder Woman", "wonder-woman.png"]
+].each do |movie_title, file_name|
+  movie = Movie.find_by!(title: movie_title)
+  file = File.open(Rails.root.join("app/assets/images/#{file_name}"))
+  movie.main_image.attach(io: file, filename: file_name)
+end
 
 Genre.create(name: "Action")
 Genre.create(name: "Comedy")
